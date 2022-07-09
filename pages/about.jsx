d0fa213b97bd9b4b060ea0f3aa6559c699aa716e
majidAbt <majid.abt98@gmail.com>
2022-07-09 09:16:10 +0430

    create about page

---
+++ pages/about.jsx
@@ -0,0 +1,28 @@
+import Particles from "react-tsparticles";
+import AnimatedText from "../components/AnimatedText";
+import SkillsCanvas from "../components/SkillsCanvas"
+import { loadFull } from "tsparticles";
+
+import styles from "./About.module.css";
+function Home() {
+  const name = "Me,\u00A0Myself\u00A0&\u00A0I";
+  const desc =
+    "I’m a Front-End Developer located in Iran. I have a serious passion for UI effects, animations and creating intuitive, dynamic user experiences. Well-organised person, problem solver, independent employee with high attention to detail. Fan of MMA, outdoor activities, TV series and English literature. Interested in the entire frontend dspectrum and working on ambitious projects with positive people.";
+
+  return (
+    <div className="h-full text-primaryText grid grid-cols-2 gap-2">
+      <div className="flex-col self-center justify-center pl-8 relative">
+        <AnimatedText text={name} />
+        <p className={`text-textPrimary ${styles.paragraph}`}>
+          {desc}
+          <p className="mt-2 text-primary cursor-pointer">
+            For more information click here!
+          </p>
+        </p>
+      </div>
+        <SkillsCanvas />
+           </div>
+  );
+}
+
+export default Home;
