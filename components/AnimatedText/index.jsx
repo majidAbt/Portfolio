069412d222b4e5cf43d45369caa458282d606291
majid.abt <majid.aboutalebi@rabex.co>
2022-10-11 16:30:39 +0330

    fix AnimatedText component fontsize responsive

--- components/AnimatedText/index.jsx
+++ components/AnimatedText/index.jsx
@@ -2,7 +2,7 @@ import styles from "./AnimatedText.module.css";
 
 export default function AnimatedText({ text }) {
   return (
-    <h1 className={`text-primary text-7xl my-8 ${styles.title} relative`}>
+    <h1 className={`text-primary sm:text-5xl text-7xl my-8 ${styles.title} relative`}>
       {text.split("").map((char, idx) => (
         <span key={char + idx} className={styles.animated}>
           {char}
