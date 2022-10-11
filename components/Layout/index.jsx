e57bbd6c2bd3c4248fb9fe7fbc4ddf291fe8af7b
majid.abt <majid.aboutalebi@rabex.co>
2022-10-11 16:47:08 +0330

    some changes in Layout & Sidebar component

--- components/Layout/index.jsx
+++ components/Layout/index.jsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
 import Sidebar from "../Sidebar";
 import styles from "./Layout.module.css";
+
 export default function Layout({ children }) {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
   return (
     <>
-      <Sidebar />
-      <main
-        className={`min-h-screen ml-[140px] bg-[#1d1d1d] ${styles.container}`}
+      <Sidebar isSidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      <div
+        className={` md:ml-[140px] lg:ml-[140px] xl:ml-[140px] bg-[#1d1d1d] relative ${styles.container}`}
       >
-        <div className={`p-4 h-full static ${styles.containerIner}`}>
+        <div className={`p-4 h-full static relative ${styles.containerIner}`}>
           {children}
         </div>
-      </main>
+      </div>
     </>
   );
 }
--- components/Sidebar/index.jsx
+++ components/Sidebar/index.jsx
@@ -1,20 +1,24 @@
 import Logo from "./../../public/assets/images/majid-logo.png";
-import LinkdinIcon from "./../../public/assets/icons/linkdin.svg";
-import GithubIcon from "./../../public/assets/icons/github.svg";
-import TwitterIcon from "./../../public/assets/icons/twitter.svg";
+import MenuIcon from "../../public/assets/icons/MenuIcon";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import CloseIcon from "../../public/assets/icons/CloseIcon";
+import { useBreakpoint } from "../../hooks/useBreakpoint";
+import Linkedin from "../../public/assets/icons/Linkedin";
+import Github from "../../public/assets/icons/Github";
+import Twitter from "../../public/assets/icons/Twitter";
 
-export default function Sidebar() {
+export default function Sidebar({ isSidebarOpen, setSidebarOpen }) {
   const { pathname } = useRouter();
+  const { isMd } = useBreakpoint('md');
   const owner = { name: "Majid", role: "Web Developer" };
-
-  return (
-    <main className="w-[140px] h-full bg-stone-800 flex flex-col justify-between xs:w-full fixed z-[2]">
+  {
+    return !isMd || isSidebarOpen ? <main className="lg:w-[140px] md:w-[140px] xl:w-[140px] h-full bg-stone-800 flex flex-col justify-between w-full fixed z-[2]">
       <div className="h-[160px] bg-[black] flex-col items-center justify-center text-center">
+        {!isMd && <CloseIcon className="w-[15px] absolute top-[15px] right-[20px] fill-primary" onClick={() => setSidebarOpen(false)} />}
         <div className="w-[70px] m-auto mt-2">
-          <Image src={Logo} style={{ width: "100%", height: "100%" }} />
+          <Image src={Logo} alt="logo" style={{ width: "100%", height: "100%" }} />
         </div>
         <h2 className="text-white font-bold mt-1 text-xl text-primaryText">
           {owner["name"]}
@@ -23,30 +27,33 @@ export default function Sidebar() {
       </div>
       <div>
         {pages.map((item) => (
-          <Link key={item.id} href={item.route}>
+          // eslint-disable-next-line @next/next/link-passhref
+          <Link key={item.id} href={item.route} >
             <p
-              className={`py-3 text-center hover:text-primary border-b-[1px] border-b-slate-400 cursor-pointer ${
-                pathname == item.route ? "text-primary" : "text-white"
-              }  hover:animate-pulse`}
+              onClick={() => setSidebarOpen(false)}
+              className={`py-3 text-center hover:text-primary border-b-[1px] border-b-slate-400 cursor-pointer ${pathname == item.route ? "text-primary" : "text-white"
+                }  hover:animate-pulse`}
             >
               {item.label}
             </p>
           </Link>
         ))}
       </div>
-      <div className="h-[150px] flex justify-center">
+      <div className="h-[80px] flex justify-center">
         <div className="flex gap-3">
           {socialMedias.map((item, idx) => (
-            <Image
-              key={item.url + idx}
-              src={item.icon}
-              className="text-white cursor-pointer"
-            />
+            <a key={idx} href={item.url} className="hover:fill-primary">
+              {item.icon}
+            </a>
+
           ))}
         </div>
       </div>
-    </main>
-  );
+    </main > : <MenuIcon className="w-[15px] absolute top-[15px] right-[20px] fill-primary" onClick={() => setSidebarOpen(false)} />
+
+
+  }
+
 }
 
 const pages = [
@@ -54,11 +61,6 @@ const pages = [
     id: 0,
     label: "About",
     route: "/about",
-  },
-  {
-    id: 1,
-    label: "My Skills",
-    route: "/mySkills",
   },
   {
     id: 2,
@@ -78,7 +80,7 @@ const pages = [
 ];
 
 const socialMedias = [
-  { id: "linkdin", url: "", icon: LinkdinIcon },
-  { id: "github", url: "", icon: GithubIcon },
-  { id: "Twiiter", url: "", icon: TwitterIcon },
+  { id: "Twitter", url: "", icon: <Twitter /> },
+  { id: "Linkedin", url: "", icon: <Linkedin /> },
+  { id: "Github", url: "", icon: <Github /> },
 ];
