b0b25325e4682f13a06dd38ab8bbc2a1d0af661f
majid.abt <majid.abt98@gmail.com>
2022-10-18 16:40:08 +0330

    fix children prop of Text component

--- components/SkillsCanvas/Word.jsx
+++ components/SkillsCanvas/Word.jsx
@@ -1,26 +1,28 @@
-import {Color } from "three";
+import { Color } from "three";
 import { Text } from '@react-three/drei'
-import {  useFrame } from '@react-three/fiber'
+import { useFrame } from '@react-three/fiber'
 import { useRef, useState, useEffect } from 'react'
 
 export default function Word({ children, ...props }) {
-    const color = new Color()
-    const fontProps = { font: '/Inter-Bold.woff', fontSize: 2.5, letterSpacing: -0.05, lineHeight: 1, 'material-toneMapped': false }
-    const ref = useRef()
-    const [hovered, setHovered] = useState(false)
-    const over = (e) => (e.stopPropagation(), setHovered(true))
-    const out = () => setHovered(false)
-    // Change the mouse cursor on hover
-    useEffect(() => {
-      if (hovered) document.body.style.cursor = 'pointer'
-      return () => (document.body.style.cursor = 'auto')
-    }, [hovered])
-    // Tie component to the render-loop
-    useFrame(({ camera }) => {
-      // Make text face the camera
-      ref.current.quaternion.copy(camera.quaternion)
-      // Animate font color
-      ref.current.material.color.lerp(color.set(hovered ? 'white' : '#08fdd8'), 0.1)
-    })
-    return <Text  ref={ref} onPointerOver={over} onPointerOut={out} {...props} {...fontProps} children={children} />
-  }
+  const color = new Color()
+  const fontProps = { font: '/Inter-Bold.woff', fontSize: 2.5, letterSpacing: -0.05, lineHeight: 1, 'material-toneMapped': false }
+  const ref = useRef()
+  const [hovered, setHovered] = useState(false)
+  const over = (e) => (e.stopPropagation(), setHovered(true))
+  const out = () => setHovered(false)
+  // Change the mouse cursor on hover
+  useEffect(() => {
+    if (hovered) document.body.style.cursor = 'pointer'
+    return () => (document.body.style.cursor = 'auto')
+  }, [hovered])
+  // Tie component to the render-loop
+  useFrame(({ camera }) => {
+    // Make text face the camera
+    ref.current.quaternion.copy(camera.quaternion)
+    // Animate font color
+    ref.current.material.color.lerp(color.set(hovered ? 'white' : '#08fdd8'), 0.1)
+  })
+  return <Text ref={ref} onPointerOver={over} onPointerOut={out} {...props} {...fontProps} >
+    {children}
+  </Text>
+}
