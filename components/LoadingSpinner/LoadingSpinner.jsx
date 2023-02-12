6e3c04161265224b596ee263eac0002b527efc6f
Majid <majid.abt98@gmail.com>
2023-02-12 09:41:23 +0330

    implement loadingSpinner component

---
+++ components/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import styles from "./LoadingSpinner.module.css"
+export default function LoadingSpinner() {
+  return (
+<div className={styles.spinner}><div></div><div></div><div></div><div></div></div>  )
+}
+
